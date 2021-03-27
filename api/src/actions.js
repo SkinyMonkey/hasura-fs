21b87461aa3e9bb5d6fea261a16fe18a4a2cf874
Adrien Candiotti <adrien.candiotti@gmail.com>
2021-03-27 02:38:47 -0400

    SQUASHME - mkdirp rewrite

--- api/src/actions.js
+++ api/src/actions.js
@@ -1,32 +1,30 @@
 const api = require('./api');
+const normalize = require('path').normalize
 
 function mkdirp(req, res) {
-  const { parent_id, path } = req.body.input;
+  const token = req.headers.authorization
+  const { parent_id, path } = req.body.input
 
-  const pathSplit = path.split('/')
+  const pathSplit = normalize(path).split('/')
 
-  pathSplit.forEach((subpath) => {
-    if (!subpath) return
-    const folder = api.createFolder(req.headers.authorization, parent_id, subpath)
-                      .catch((err) => {
-                        // FIXME : check errors
-                        console.error(err)
-                        res.status(500).send({err: 'Internal error'})
-                      })
-    parent_id = folder.id
+  // TODO : check if path size > 0
+
+  const firstPromise = api.createFolder(token, parent_id, pathSplit[0])
+
+  pathSplit.slice(1).reduce((previousPromise, subpath) => {
+    return previousPromise.then(folder => api.createFolder(token, folder.id, subpath))
+  }, firstPromise)
+  .then(folder => res.json({id: folder.id}))
+  .catch(err => {
+    console.error(err)
+    // TODO : same format as the one returned by hasura?
+    // TODO : check if error not a user one? can we prevent writes to parent_id if not owned for example?
+    res.status(500).send({errors: ['Internal error']})
   })
+}
 
-  // FIXME: DEBUG
-  return
-
-  // success
-  return res.json({
-    id: "<value>"
-  })
-};
-
-mkdirp({body: {input: {parent_id: "abc", path: "/a/b/c"}}})
-mkdirp({body: {input: {parent_id: "abc", path: "a/b/c"}}})
-mkdirp({body: {input: {parent_id: "abc", path: "a/b//c"}}})
+//mkdirp({body: {input: {parent_id: "abc", path: "/a/b/c"}}})
+//mkdirp({body: {input: {parent_id: "abc", path: "a/b/c"}}})
+//mkdirp({body: {input: {parent_id: "abc", path: "a/b//c"}}})
 
 exports.mkdirp = mkdirp;
